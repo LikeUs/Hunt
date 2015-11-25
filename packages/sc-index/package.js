1ae9178d80565cab67cc7e2286fe5395e1f473f9
Max Tilford <maxtilford@gmail.com>
2015-11-25 12:18:40 -0800

    Add index package

---
+++ packages/sc-index/package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'sc-index',
+  version: '0.0.1'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('templating');
+
+  api.addFiles('index.html');
+});
+
+Package.onTest(function(api) {
+});
